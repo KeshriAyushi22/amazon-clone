71cddcbb386ca4d63a4c35366219900c032c0ebc
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-09-09 05:39:15 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5n0MoPVWqpCrHX30WgSnRylE3SYJNfkA",
+    authDomain: "clone-b6acf.firebaseapp.com",
+    databaseURL: "https://clone-b6acf.firebaseio.com",
+    projectId: "clone-b6acf",
+    storageBucket: "clone-b6acf.appspot.com",
+    messagingSenderId: "308030533419",
+    appId: "1:308030533419:web:dccf794f28a0c28f799f6e",
+    measurementId: "G-SXX8KE6WGL"
+};
