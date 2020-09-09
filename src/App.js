aeba426d1919c4a4d4a28934651d4860bd4de24c
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-09-09 08:27:00 +0530

    header css and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>hello</h1>
+      <Header />
     </div>
   );
 }
