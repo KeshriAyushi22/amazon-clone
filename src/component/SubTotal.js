e7ee1815619cc9b9ee78f3a6d5d39302a88f0eb7
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-09-10 08:40:27 +0530

    subtotal left and right css

--- src/component/SubTotal.js
+++ src/component/SubTotal.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import "./Subtotal.css"
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { ShoppingBasket } from "@material-ui/icons";
 
 function SubTotal() {
     return (
         <div className="subtotal">
-
+            <CurrencyFormat
+                renderText={(value) => {
+                    <>
+                        <p>
+                            Subtotal ({basket.total} items) :<strong>{`${value}`}</strong>
+                            <small className="subtotal__gift">
+                                <input type="checkbox" />
+                This order conatins a gift
+              </small>
+                        </p>
+                    </>;
+                }}
+                decimalScale={2}
+                value={getBasketTotal(basket)}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}  //need to fetch it accordingly
+            />
+            <button>Proceed to Checkout</button>
         </div>
-    )
+    );
 }
 
-export default SubTotal
+export default SubTotal;
