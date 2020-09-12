cd0df5acef6005f118bb9fb707833f8ed39e9ba9
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-09-12 23:47:08 +0530

    deploying changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-b6acf/us-central1/api/'  //the api client url
+    // baseURL: 'http://localhost:5001/clone-b6acf/us-central1/api/'
+    baseURL: 'https://us-central1-clone-b6acf.cloudfunctions.net/api'
+
+    //the api client url
 })
 
 export default instance
